f0897afea8afda3da2521fbd2f5b76e3e75999bb
MaximeVDG <Max@MacBook-Air-de-Maxime.local>
2014-12-22 12:15:05 +0900

    Rebuilt using CoreData

--- TaskIt/TaskModel.swift
+++ TaskIt/TaskModel.swift
@@ -2,15 +2,19 @@
 //  TaskModel.swift
 //  TaskIt
 //
-//  Created by Maxime Vandegeerde on 16/12/14.
+//  Created by Maxime Vandegeerde on 21/12/14.
 //  Copyright (c) 2014 Maxime Vandegeerde. All rights reserved.
 //
 
 import Foundation
+import CoreData
 
-struct TaskModel {
-    var task: String
-    var subTask: String
-    var date: NSDate
-    var completed: Bool
+@objc(TaskModel)
+class TaskModel: NSManagedObject {
+
+    @NSManaged var completed: NSNumber
+    @NSManaged var date: NSDate
+    @NSManaged var subtask: String
+    @NSManaged var task: String
+
 }
